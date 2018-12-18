88e34ae7686735454279d9d3f68001c72c4794fe
unknown <wadimsolowiow@gmail.com>
2018-12-18 15:15:29 +0200

    opacity fix

--- src/app/routes/homepage/index.js
+++ src/app/routes/homepage/index.js
@@ -1,81 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid, Image, Segment } from 'semantic-ui-react';
 import Slider from 'react-slick';
 import Page from '../../components/page';
 
 import './homepage.scss';
 
-const settings = {
-	// infinite: false,
-	className: 'slider variable-width',
-	dots: false,
-	infinite: true,
-	centerMode: true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	variableWidth: true,
-	autoplay: true,
-	autoplaySpeed: 4000,
-};
-export default () => (
-	<Page id="homepage">
-		<div>
-			<Slider {...settings}>
-				<div className="slider-item">
-					<Image src={require('../../assets/1.jpg')} />
-					<div className="slider-text">Top apple product</div>
-				</div>
-				<div className="slider-item">
-					<Image src={require('../../assets/2.jpg')} />
-					<div className="slider-text">Top apple product</div>
-				</div>
-				<div className="slider-item">
-					<Image src={require('../../assets/4.jpg')} />
-					<div className="slider-text">Top apple product</div>
-				</div>
-				<div className="slider-item">
-					<Image src={require('../../assets/5.jpg')} />
-					<div className="slider-text">Top apple product</div>
-				</div>
-			</Slider>
-		</div>
+export default class HomePage extends Component {
+	state = {
+		loaded: false,
+	};
 
-		<Grid stackable columns={2}>
-			<Grid.Column>
-				<Segment>
-					<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-				</Segment>
-			</Grid.Column>
-			<Grid.Column>
-				<Segment>
-					<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-				</Segment>
-			</Grid.Column>
-			<Grid.Column>
-				<Segment>
-					<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-				</Segment>
-			</Grid.Column>
-			<Grid.Column>
-				<Segment>
-					<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-				</Segment>
-			</Grid.Column>
-			<Grid.Column>
-				<Segment>
-					<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-				</Segment>
-			</Grid.Column>
-			<Grid.Column>
-				<Segment>
-					<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-				</Segment>
-			</Grid.Column>
-			<Grid.Column>
-				<Segment>
-					<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
-				</Segment>
-			</Grid.Column>
-		</Grid>
-	</Page>
-);
+	render() {
+		const settings = {
+			// infinite: false,
+			className: 'slider variable-width',
+			dots: false,
+			infinite: true,
+			centerMode: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			variableWidth: true,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			onInit: () => {
+				this.setState({
+					loaded: true,
+				});
+			},
+		};
+
+		return (
+			<Page id="homepage">
+				<div
+					className="slider-wrapper"
+					style={{
+						opacity: this.state.loaded ? 1 : 0,
+					}}
+				>
+					<Slider {...settings}>
+						<div className="slider-item">
+							<Image src={require('../../assets/1.jpg')} />
+							<div className="slider-text">Top apple product</div>
+						</div>
+						<div className="slider-item">
+							<Image src={require('../../assets/2.jpg')} />
+							<div className="slider-text">Top apple product</div>
+						</div>
+						<div className="slider-item">
+							<Image src={require('../../assets/4.jpg')} />
+							<div className="slider-text">Top apple product</div>
+						</div>
+						<div className="slider-item">
+							<Image src={require('../../assets/5.jpg')} />
+							<div className="slider-text">Top apple product</div>
+						</div>
+					</Slider>
+				</div>
+
+				<Grid stackable columns={2}>
+					<Grid.Column>
+						<Segment>
+							<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+						</Segment>
+					</Grid.Column>
+					<Grid.Column>
+						<Segment>
+							<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+						</Segment>
+					</Grid.Column>
+					<Grid.Column>
+						<Segment>
+							<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+						</Segment>
+					</Grid.Column>
+					<Grid.Column>
+						<Segment>
+							<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+						</Segment>
+					</Grid.Column>
+					<Grid.Column>
+						<Segment>
+							<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+						</Segment>
+					</Grid.Column>
+					<Grid.Column>
+						<Segment>
+							<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+						</Segment>
+					</Grid.Column>
+					<Grid.Column>
+						<Segment>
+							<Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+						</Segment>
+					</Grid.Column>
+				</Grid>
+			</Page>
+		);
+	}
+}
